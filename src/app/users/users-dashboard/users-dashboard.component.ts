7351f489ff13c4e75663cbf48672d1f16d95bd4a
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2024-09-12 11:48:03 -0600

    add: se añade vista mejorada en dashboard y envio de datos

--- src/app/users/users-dashboard/users-dashboard.component.ts
+++ src/app/users/users-dashboard/users-dashboard.component.ts
@@ -1,24 +1,41 @@
 import { Component } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { IUser } from '../interfaces/iuser';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-users-dashboard',
   templateUrl: './users-dashboard.component.html',
   styleUrl: './users-dashboard.component.css'
 })
-export class UsersDashboardComponent {
+export class UsersDashboardComponent implements OnInit{
+
+  ngOnInit(): void {
+      this._service.getAll().subscribe(
+        response => this.users = response
+      )
+  }
+
+  selectedUser: IUser = {
+    id: 1,
+    name: "Marco",
+    username: "marco1",
+    email: "marco1@gmail.com",
+    phone: "961 872 9982",
+    website: "marco1.com"
+  }
+
+  displayedColumns: string[] = ['id', 'name', 'username', 'email', 'phone', 'website']
 
   users: IUser[] = []
 
-  constructor(private _service: UserService){  
+  constructor(private _service: UserService){
 
-    this._service.getAll().subscribe(
-      response => {
-        this.users = response
-      }
-    )
+  }
 
+  addUser(user: IUser): void{
+    this.users.push(user);
+    console.log(this.users);
   }
 
 }
